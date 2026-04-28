3e142619d78bab9395d6da65d50e698eb79c813c
adilson.zulvi <adilson.zulvi@matrixintegrasi.com>
2026-04-28 11:30:50 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
     host: "0.0.0.0",
     port: 59989,
